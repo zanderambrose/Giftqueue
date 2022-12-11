1771456dd4a568e7b0def0c6838bfc36d039a08b
Zander Ambrose <alexander.d.ambrose@gmail.com>
2022-12-11 12:10:45 -0500

    Fix version incompatibility.

--- registry_be/api/views.py
+++ registry_be/api/views.py
@@ -1,12 +1,6 @@
-from rest_framework import viewsets
-from .models import RegistryUser
-from .serializer import UserCreateSerializer
-from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework_simplejwt.authentication import JWTAuthentication
 
 class TestView(APIView):
 
