b08502a74af2b831a4abbd084a6f5198f5781ae2
Zander Ambrose <alexander.d.ambrose@gmail.com>
2022-12-11 19:56:41 -0500

    Smaller profile image.

--- registry_fe/components/ProfileDropdown.tsx
+++ registry_fe/components/ProfileDropdown.tsx
@@ -23,8 +23,8 @@ const ProfileDropdown = () => {
           : "https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o="
       }
       alt="UserName profile image"
-      width={50}
-      height={50}
+      width={40}
+      height={40}
       className="rounded-circle"
     />
   );
