155429d2a92294ba156cb8421084034a6f405efc
Zander Ambrose <alexander.d.ambrose@gmail.com>
2022-12-15 09:16:15 -0500

    WIP.

--- registry_fe/components/AddGiftItem.tsx
+++ registry_fe/components/AddGiftItem.tsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import InputGroup from "react-bootstrap/InputGroup";
+import { ArrowRight, X } from "react-bootstrap-icons";
 
 const AddGiftItem = () => {
+  const [urlArr, setUrlArr] = useState<string[]>([""]);
+  const handleClearUrlClick = () => {
+    console.log("handle click called");
+    setUrlArr((currVal) => {
+      return currVal.filter((item) => {
+        return item !== "";
+      });
+    });
+  };
+  const handleAddUrlClick = () => {
+    if (urlArr.at(-1) === "") {
+      alert("fill in the first url first boi");
+    } else {
+      setUrlArr((currVal) => [...currVal, ""]);
+    }
+  };
   return (
     <>
       <Form>
@@ -10,11 +28,30 @@ const AddGiftItem = () => {
           <Form.Label>What is your wishlist item</Form.Label>
           <Form.Control type="text" placeholder="Enter item name" />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Enter a url</Form.Label>
-          <Form.Control type="text" placeholder="Enter url here" />
-        </Form.Group>
-        <Button variant="secondary" type="button">
+        {urlArr.map((item) => {
+          return (
+            <div key={urlArr.length}>
+              <Form.Label>Enter a url {urlArr.length}</Form.Label>
+              <InputGroup className="mb-3">
+                <Form.Control
+                  onChange={(e) => {
+                    console.log(e.target.value);
+                  }}
+                  aria-label="Amount (to the nearest dollar)"
+                />
+                <InputGroup.Text onClick={() => handleClearUrlClick()}>
+                  <X color="red" size={20} />
+                </InputGroup.Text>
+              </InputGroup>
+            </div>
+          );
+        })}
+        <Button
+          onClick={() => handleAddUrlClick()}
+          className="mb-3"
+          variant="secondary"
+          type="button"
+        >
           Add url
         </Button>
         <br />
