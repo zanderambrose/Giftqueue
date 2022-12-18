8aa65931fce4ce69b434bc7c9210f8f624a547a4
Zander Ambrose <alexander.d.ambrose@gmail.com>
2022-12-18 09:09:06 -0500

    Remove is_purchased field from owners UI.

--- registry_be/api/serializer.py
+++ registry_be/api/serializer.py
@@ -30,6 +30,17 @@ class GiftItemUrlSerializer(serializers.ModelSerializer):
 
 class GiftItemAllSerializer(serializers.ModelSerializer):
     url = GiftItemUrlSerializer(read_only=True, many=True, source='giftitemurl_set')
+   
+    def to_representation(self, instance):
+        ret = super(GiftItemAllSerializer, self).to_representation(instance)
+
+        # remove 'is_purchased' field if owner is requestings
+        user_id = self.context['request'].user.id
+        if user_id and user_id == ret['owner']:
+            del ret['is_purchased']
+
+        # return the modified representation
+        return ret
 
 
     class Meta:
