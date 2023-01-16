4194760b38454a5664ea7354907928ea5fcfa00f
Zander Ambrose <alexander.d.ambrose@gmail.com>
2023-01-16 12:14:25 -0500

    Create post save signals on day and gift to create activity rows.

--- registry_be/api/signals.py
+++ registry_be/api/signals.py
@@ -1,7 +1,7 @@
 import datetime
-from django.db.models.signals import pre_save 
+from django.db.models.signals import pre_save, post_save 
 from django.dispatch import receiver
-from .models import Friendship 
+from .models import Friendship, CelebrationDay, GiftItem, ActivityFeed
 
 @receiver(pre_save, sender=Friendship)
 def handle_accepted(sender, instance, **kwargs):
@@ -13,4 +13,14 @@ def handle_accepted(sender, instance, **kwargs):
 
     if is_previously_accepted is not None and is_previously_accepted.is_accepted == False and instance.is_accepted == True:
         instance.date_accepted = datetime.datetime.now()
-        
+
+
+@receiver(post_save, sender=CelebrationDay)
+def handle_celebrationday_activity(sender, instance, created, **kwargs):
+    if created:
+        ActivityFeed.objects.create(owner=instance.owner, action="DAY")
+
+@receiver(post_save, sender=GiftItem)
+def handle_giftitem_activity(sender, instance, created, **kwargs):
+    if created:
+        ActivityFeed.objects.create(owner=instance.owner, action='GIFT')
