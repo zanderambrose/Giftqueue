61928dffae0d4147d550312eac7347a5db886b01
Zander Ambrose <alexander.d.ambrose@gmail.com>
2023-01-22 11:27:44 -0500

    Navigation state visually changing on click.

--- frontend/components/Navigation.tsx
+++ frontend/components/Navigation.tsx
@@ -1,23 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRightFromBracket } from "@fortawesome/free-solid-svg-icons";
 
+type TNavOptions = "day" | "giftqueue" | "friends" | "calendar";
 const Navigation = () => {
+  const [navState, setNavState] = useState<TNavOptions>("day");
+  const handleNavStateChange = (navItemState: TNavOptions) => {
+    setNavState(navItemState);
+  };
   return (
     <nav className="h-full flex items-center border-2 border-black">
       <div className="sidebar-layout">
         <Image src="/giftqueueLogo.png" width="78px" height="56px" />
       </div>
       <div className="flex-1 flex justify-around">
-        <h3 className="nav-link nav-link-grow-up">Celebration Day</h3>
-        <h3 className="nav-link nav-link-grow-up">Giftqueue</h3>
-        <h3 className="nav-link nav-link-grow-up">Friends</h3>
-        <h3 className="nav-link nav-link-grow-up">Calendar</h3>
+        <h3
+          className={
+            navState === "day" ? "nav-link nav-link-grow-up" : "nav-link"
+          }
+          onClick={() => handleNavStateChange("day")}
+        >
+          Celebration Day
+        </h3>
+        <h3
+          className={
+            navState === "giftqueue" ? "nav-link nav-link-grow-up" : "nav-link"
+          }
+          onClick={() => handleNavStateChange("giftqueue")}
+        >
+          Giftqueue
+        </h3>
+        <h3
+          className={
+            navState === "friends" ? "nav-link nav-link-grow-up" : "nav-link"
+          }
+          onClick={() => handleNavStateChange("friends")}
+        >
+          Friends
+        </h3>
+        <h3
+          className={
+            navState === "calendar" ? "nav-link nav-link-grow-up" : "nav-link"
+          }
+          onClick={() => handleNavStateChange("calendar")}
+        >
+          Calendar
+        </h3>
       </div>
       <div className="sidebar-layout">
         <div className="text-center">
-          <div className="flex justify-center items-center">
+          <div className="flex justify-center items-center cursor-pointer">
             <FontAwesomeIcon icon={faArrowRightFromBracket} />
             <div className="ml-2">Logout</div>
           </div>
