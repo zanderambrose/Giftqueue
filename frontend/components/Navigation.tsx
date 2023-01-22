e9e5d17f87bc4601245ca4dbdb41b2c1d413c461
Zander Ambrose <alexander.d.ambrose@gmail.com>
2023-01-22 09:39:37 -0500

    Navigation work.

--- frontend/components/Navigation.tsx
+++ frontend/components/Navigation.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import Image from "next/image";
 
 const Navigation = () => {
-  return <nav className="h-full border-2 border-black">Navigation</nav>;
+  return (
+    <nav className="h-full flex items-center border-2 border-black">
+      <div className="sidebar-layout">
+        <Image src="/giftqueueLogo.png" width="78px" height="56px" />
+      </div>
+      <div className="flex-1 flex justify-around">
+        <h3>Celebration Day</h3>
+        <h3>Giftqueue</h3>
+        <h3>Friends</h3>
+        <h3>Calendar</h3>
+      </div>
+      <div className="sidebar-layout">
+        <div className="text-center">Layout</div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navigation;
