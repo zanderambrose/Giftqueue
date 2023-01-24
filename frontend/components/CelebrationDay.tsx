282d2b4598e809a1b5690b86adacd8a72b2d678e
Zander Ambrose <alexander.d.ambrose@gmail.com>
2023-01-24 07:28:46 -0500

    Add font awesome X icon to modal.

--- frontend/components/CelebrationDay.tsx
+++ frontend/components/CelebrationDay.tsx
@@ -1,4 +1,5 @@
-import { faChampagneGlasses } from "@fortawesome/free-solid-svg-icons";
+import { faChampagneGlasses, faX } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
 import NoItemDefaultCard from "./NoItemDefaultCard";
 
@@ -30,11 +31,11 @@ const CelebrationDay = () => {
                 <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
                   <h3 className="text-3xl font-semibold">Celebration Day</h3>
                   <button
-                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    className="p-1 ml-auto bg-transparent border-0 text-black float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
                     onClick={() => setAddFirstDayModal(false)}
                   >
                     <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                      ×
+                      <FontAwesomeIcon icon={faX} />
                     </span>
                   </button>
                 </div>
--- frontend/components/Giftqueue.tsx
+++ frontend/components/Giftqueue.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faListCheck } from "@fortawesome/free-solid-svg-icons";
+import { faListCheck, faX } from "@fortawesome/free-solid-svg-icons";
 import NoItemDefaultCard from "./NoItemDefaultCard";
 
 const Giftqueue = () => {
@@ -30,13 +30,15 @@ const Giftqueue = () => {
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                 {/*header*/}
                 <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                  <h3 className="text-3xl font-semibold">GIFTQUEUE</h3>
+                  <h3 className="text-3xl font-semibold">
+                    Add new item to your Giftqueue
+                  </h3>
                   <button
-                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    className="p-1 ml-auto bg-transparent border-0 text-black float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
                     onClick={() => setAddFirstItemModal(false)}
                   >
                     <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                      ×
+                      <FontAwesomeIcon icon={faX} />
                     </span>
                   </button>
                 </div>
