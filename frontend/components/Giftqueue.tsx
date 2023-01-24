11428da15dc3ecf3539af253f64eaa9e8baa61ed
Zander Ambrose <alexander.d.ambrose@gmail.com>
2023-01-24 07:40:07 -0500

    Style modal confirm button and begin giftqueue modal style.

--- frontend/components/Giftqueue.tsx
+++ frontend/components/Giftqueue.tsx
@@ -29,8 +29,8 @@ const Giftqueue = () => {
               {/*content*/}
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                 {/*header*/}
-                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                  <h3 className="text-3xl font-semibold">
+                <div className="flex items-start justify-between p-5">
+                  <h3 className="text-2xl font-semibold">
                     Add new item to your Giftqueue
                   </h3>
                   <button
@@ -43,8 +43,8 @@ const Giftqueue = () => {
                   </button>
                 </div>
                 {/*body*/}
-                <div className="relative p-6 flex-auto">
-                  <p className="my-4 text-slate-500 text-lg leading-relaxed">
+                <div className="relative px-6 flex-auto">
+                  <p className="text-slate-500 text-lg leading-relaxed">
                     I always felt like I could do anything. That’s the main
                     thing people are controlled by! Thoughts- their perception
                     of themselves! They're slowed down by their perception of
@@ -53,20 +53,13 @@ const Giftqueue = () => {
                   </p>
                 </div>
                 {/*footer*/}
-                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                <div className="text-center block p-6">
                   <button
-                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    className="main-Btn hover:opacity-80"
                     type="button"
                     onClick={() => setAddFirstItemModal(false)}
                   >
-                    Close
-                  </button>
-                  <button
-                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                    onClick={() => setAddFirstItemModal(false)}
-                  >
-                    Save Changes
+                    Confirm
                   </button>
                 </div>
               </div>
