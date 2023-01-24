b49935b62464a0b871adbf2a40894f76cbd2d320
Zander Ambrose <alexander.d.ambrose@gmail.com>
2023-01-24 07:18:54 -0500

    Get giftqueue and celebration sections rendering seperate modals.

--- frontend/components/Giftqueue.tsx
+++ frontend/components/Giftqueue.tsx
@@ -4,7 +4,12 @@ import { faListCheck } from "@fortawesome/free-solid-svg-icons";
 import NoItemDefaultCard from "./NoItemDefaultCard";
 
 const Giftqueue = () => {
+  // State for showing users giftqueue or default state
   const [hasGifts, setHasGifts] = useState<boolean>(false);
+
+  //State for showing default add item modal
+  const [addFirstItemModal, setAddFirstItemModal] = useState(false);
+
   return (
     <>
       {!hasGifts ? (
@@ -12,10 +17,62 @@ const Giftqueue = () => {
           icon={faListCheck}
           headingText="No items in your giftqueue"
           subText="Let your friends know the items you wish to have."
+          setModalToShow={setAddFirstItemModal}
         />
       ) : (
         <h2>you got elements</h2>
       )}
+      {addFirstItemModal ? (
+        <>
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                  <h3 className="text-3xl font-semibold">GIFTQUEUE</h3>
+                  <button
+                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    onClick={() => setAddFirstItemModal(false)}
+                  >
+                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                      ×
+                    </span>
+                  </button>
+                </div>
+                {/*body*/}
+                <div className="relative p-6 flex-auto">
+                  <p className="my-4 text-slate-500 text-lg leading-relaxed">
+                    I always felt like I could do anything. That’s the main
+                    thing people are controlled by! Thoughts- their perception
+                    of themselves! They're slowed down by their perception of
+                    themselves. If you're taught you can’t do anything, you
+                    won’t do anything. I was taught I could do everything.
+                  </p>
+                </div>
+                {/*footer*/}
+                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                  <button
+                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setAddFirstItemModal(false)}
+                  >
+                    Close
+                  </button>
+                  <button
+                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setAddFirstItemModal(false)}
+                  >
+                    Save Changes
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
     </>
   );
 };
