494da2459402da563b508ba70d3d936f5c12088d
Zander Ambrose <alexander.d.ambrose@gmail.com>
2023-01-24 08:37:13 -0500

    Begin add new calendar day modal.

--- frontend/components/CelebrationDay.tsx
+++ frontend/components/CelebrationDay.tsx
@@ -24,46 +24,48 @@ const CelebrationDay = () => {
       {addFirstDayModal ? (
         <>
           <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+            <div className="relative w-4/12 my-6 mx-auto max-w-3xl">
               {/*content*/}
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                 {/*header*/}
-                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                  <h3 className="text-3xl font-semibold">Celebration Day</h3>
+                <div className="flex items-start justify-between p-5">
+                  <h3 className="text-lg font-semibold">Add New Event</h3>
                   <button
                     className="p-1 ml-auto bg-transparent border-0 text-black float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
                     onClick={() => setAddFirstDayModal(false)}
                   >
-                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                      <FontAwesomeIcon icon={faX} />
+                    <span className="bg-transparent text-black h-6 w-6 text-2xl block outline-none focus:outline-none">
+                      <FontAwesomeIcon className="text-black" icon={faX} />
                     </span>
                   </button>
                 </div>
                 {/*body*/}
-                <div className="relative p-6 flex-auto">
-                  <p className="my-4 text-slate-500 text-lg leading-relaxed">
-                    I always felt like I could do anything. That’s the main
-                    thing people are controlled by! Thoughts- their perception
-                    of themselves! They're slowed down by their perception of
-                    themselves. If you're taught you can’t do anything, you
-                    won’t do anything. I was taught I could do everything.
-                  </p>
+                <div className="relative px-6 flex-auto">
+                  <form
+                    onSubmit={(e) => {
+                      e.preventDefault();
+                      alert("hello form submit");
+                    }}
+                  >
+                    <input
+                      type="text"
+                      placeholder="Enter Event Title"
+                      className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-md shadow-sm placeholder-slate-400
+      focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+      invalid:border-pink-500 invalid:text-pink-600
+      focus:invalid:border-pink-500 focus:invalid:ring-pink-500
+    "
+                    />
+                  </form>
                 </div>
                 {/*footer*/}
-                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                <div className="text-center block p-6">
                   <button
-                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    className="main-Btn hover:opacity-80"
                     type="button"
                     onClick={() => setAddFirstDayModal(false)}
                   >
-                    Close
-                  </button>
-                  <button
-                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                    onClick={() => setAddFirstDayModal(false)}
-                  >
-                    Save Changes
+                    Confirm
                   </button>
                 </div>
               </div>
