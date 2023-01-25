41654c7d22dc9bd0940e2e0a0980f9b6c11369ea
Zander Ambrose <alexander.d.ambrose@gmail.com>
2023-01-25 08:07:12 -0500

    Add click off modal screen to close modal.

--- frontend/components/CelebrationDay.tsx
+++ frontend/components/CelebrationDay.tsx
@@ -24,7 +24,10 @@ const CelebrationDay = () => {
       )}
       {addFirstDayModal ? (
         <>
-          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+          <div
+            onClick={() => setAddFirstDayModal(false)}
+            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+          >
             <div className="relative w-4/12 my-6 mx-auto max-w-3xl">
               {/*content*/}
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
