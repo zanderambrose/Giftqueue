d4015b474214a61119f122deb614dc1d5b700f7e
Zander Ambrose <alexander.d.ambrose@gmail.com>
2023-01-25 19:25:40 -0500

    Remove overscroll-y-scroll because of weird right padding.

--- frontend/components/Giftqueue.tsx
+++ frontend/components/Giftqueue.tsx
@@ -28,7 +28,7 @@ const Giftqueue = () => {
             onClick={() => setAddFirstItemModal(false)}
             className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
           >
-            <div className="relative overflow-y-scroll w-4/12 my-6 mx-auto max-w-3xl giftQueueModal">
+            <div className="relative w-4/12 my-6 mx-auto max-w-3xl giftQueueModal">
               {/*content*/}
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                 {/*header*/}
