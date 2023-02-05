013fd90cde86d2a3834efe66fa6e2407289bb132
Zander Ambrose <alexander.d.ambrose@gmail.com>
2023-02-05 11:53:44 -0500

    Require main nav state to be on friends when visiting friend detail page.

--- frontend/pages/[username].tsx
+++ frontend/pages/[username].tsx
@@ -2,10 +2,19 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import Layout from "../components/Layout";
+import { useEffect } from "react";
+import { useSetRecoilState } from "recoil";
+import { navigationState } from "../recoil/navigationState";
 
 const FriendProfile: NextPage = () => {
   const router = useRouter();
+  const setNavState = useSetRecoilState(navigationState);
   const { username } = router.query;
+  useEffect(() => {
+    if (router.pathname !== "/") {
+      setNavState("friends");
+    }
+  }, [router.pathname]);
   return (
     <div>
       <Head>
