d6f08dda92d0094c5657d67c0c9ad579a46c3274
Zander Ambrose <alexander.d.ambrose@gmail.com>
2023-02-06 09:21:42 -0500

    Create recoil state for delete item modal.

---
+++ frontend/recoil/modal/deleteItem.ts
@@ -0,0 +1,18 @@
+import { atom } from "recoil";
+
+export interface IDeleteItem {
+  uuid: string | undefined;
+  isOpen: boolean;
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export const defaultDeleteItemState = {
+  uuid: undefined,
+  isOpen: false,
+  setIsOpen: () => {},
+};
+
+export const giftqueueItem = atom<IDeleteItem>({
+  key: "giftqueueItem",
+  default: defaultDeleteItemState,
+});
