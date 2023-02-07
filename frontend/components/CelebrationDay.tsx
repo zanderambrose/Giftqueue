64430c7acfb292be863f8ada8a0a86ebe0019415
Zander Ambrose <alexander.d.ambrose@gmail.com>
2023-02-07 08:58:55 -0500

    Remove modal from CelebrationDay component.

--- frontend/components/CelebrationDay.tsx
+++ frontend/components/CelebrationDay.tsx
@@ -1,12 +1,7 @@
-import {
-  faChampagneGlasses,
-  faX,
-  faPlus,
-} from "@fortawesome/free-solid-svg-icons";
+import { faChampagneGlasses, faPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
 import NoItemDefaultCard from "./NoItemDefaultCard";
-import Calendar from "react-calendar";
 import DateCard from "./DateCard";
 import MyEventCard from "./MyEventCard";
 
@@ -65,68 +60,6 @@ const CelebrationDay = () => {
           </div>
         </div>
       )}
-      {addFirstDayModal ? (
-        <>
-          <div
-            onClick={() => setAddFirstDayModal(false)}
-            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
-          >
-            <div className="relative w-5/12 my-6 mx-auto max-w-3xl">
-              {/*content*/}
-              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                {/*header*/}
-                <div className="flex items-start justify-between p-5">
-                  <h3 className="text-lg font-semibold">Add New Event</h3>
-                  <button
-                    className="p-1 ml-auto bg-transparent border-0 text-black float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                    onClick={() => setAddFirstDayModal(false)}
-                  >
-                    <span className="bg-transparent text-black h-6 w-6 text-2xl block outline-none focus:outline-none">
-                      <FontAwesomeIcon className="text-black" icon={faX} />
-                    </span>
-                  </button>
-                </div>
-                {/*body*/}
-                <div className="relative px-6 flex-auto">
-                  <form
-                    onSubmit={(e) => {
-                      e.preventDefault();
-                      alert("hello form submit");
-                    }}
-                  >
-                    <input
-                      type="text"
-                      placeholder="Enter Event Title"
-                      className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-md shadow-sm placeholder-slate-400
-      focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
-      invalid:border-pink-500 invalid:text-pink-600
-      focus:invalid:border-pink-500 focus:invalid:ring-pink-500
-    "
-                    />
-                    <label className="block mt-8">
-                      <span className="block text-md font-medium">
-                        Select Date
-                      </span>
-                    </label>
-                    <Calendar />
-                  </form>
-                </div>
-                {/*footer*/}
-                <div className="text-center block p-6">
-                  <button
-                    className="main-Btn hover:opacity-80"
-                    type="button"
-                    onClick={() => setAddFirstDayModal(false)}
-                  >
-                    Confirm
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-        </>
-      ) : null}
     </>
   );
 };
