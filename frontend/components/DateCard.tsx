72d6f92d2a7ad7b79fe9d52d3db0dad638ec9655
Zander Ambrose <alexander.d.ambrose@gmail.com>
2023-02-09 21:27:57 -0500

    Get weekday rendering.

--- frontend/components/DateCard.tsx
+++ frontend/components/DateCard.tsx
@@ -1,17 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import date from "date-and-time";
 
 const DateCard = ({ date: celebrationDate }: { date: string }) => {
-  const currentYearDate = celebrationDate.replace(/^.{4}/g, "2023");
-  const formattedDate = date.format(
-    new Date(currentYearDate),
-    "ddd,MMM DD YYYY"
-  );
-  console.log(formattedDate);
+  const [weekday, setWeekday] = useState("Mon");
+  useEffect(() => {
+    // Replace the years input with current year
+    // TODO - programatically add current year instead of hardcode 2023
+    const currentYearDate = celebrationDate.replace(/^.{4}/g, "2023");
+    const formattedDate = date.format(
+      new Date(currentYearDate),
+      "ddd, MMM DD YYYY"
+    );
+    console.log("FORMATTED DATE: ", formattedDate);
+    setWeekday(formattedDate.substring(0, 3));
+  }, []);
   return (
     <div className="dateCard">
-      <p>{celebrationDate}</p>
-      <p>Sun</p>
+      <p>{weekday}</p>
       <p className="text-lg font-black">15</p>
       <p>Jan</p>
     </div>
