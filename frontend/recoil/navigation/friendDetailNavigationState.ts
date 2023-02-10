473f2b07a996e8afdb8a928c05a4f3840f4fa3e1
Zander Ambrose <alexander.d.ambrose@gmail.com>
2023-02-10 09:21:38 -0500

    Update filepath.

--- frontend/recoil/navigation/friendDetailNavigationState.ts
+++ frontend/recoil/navigation/friendDetailNavigationState.ts
@@ -1,7 +1,7 @@
 // CURRENTLY NOT USING THIS
 // UPDATE IF I NEED GLOABAL DETAIL STATE
 import { atom } from "recoil";
-import { TFriendDetailNavOptions } from "../components/FriendDetailPage";
+import { TFriendDetailNavOptions } from "../../components/FriendDetailPage";
 
 export const friendDetailNavigationState = atom<TFriendDetailNavOptions>({
   key: "friendDetailNavigationState",
