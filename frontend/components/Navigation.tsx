9222196712c1fd9a64a5d09ced560edd23880fb7
Zander Ambrose <alexander.d.ambrose@gmail.com>
2023-02-11 13:16:40 -0500

    App responds into mobile layout.

--- frontend/components/Navigation.tsx
+++ frontend/components/Navigation.tsx
@@ -8,6 +8,7 @@ import Image from "next/image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRightFromBracket } from "@fortawesome/free-solid-svg-icons";
 import { useRouter } from "next/router";
+import { faBell, faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Navigation = () => {
   const [navState, setNavState] = useRecoilState(navigationState);
@@ -27,59 +28,73 @@ const Navigation = () => {
   };
 
   return (
-    <nav className="h-full flex items-center nav-shadow">
-      <div onClick={() => handleRouteHome()} className="absolute left-2">
+    <>
+      <nav className="h-full flex items-center nav-shadow nav-mobile-hidden">
+        <div onClick={() => handleRouteHome()} className="absolute left-2">
+          <Image
+            src="/giftqueueLogo.png"
+            width={"78"}
+            height={"56"}
+            alt={"giftqueue logo"}
+          />
+        </div>
+        <div className="sidebar-layout sidebar-left-hidden"></div>
+        <div className="flex-1 flex justify-around handle-responsive-margin">
+          <h3
+            className={
+              navState === "giftqueue"
+                ? "nav-link nav-link-grow-up"
+                : "nav-link"
+            }
+            onClick={() => handleNavStateChange("giftqueue")}
+          >
+            Giftqueue
+          </h3>
+          <h3
+            className={
+              navState === "day" ? "nav-link nav-link-grow-up" : "nav-link"
+            }
+            onClick={() => handleNavStateChange("day")}
+          >
+            Celebrations
+          </h3>
+          <h3
+            className={
+              navState === "friends" ? "nav-link nav-link-grow-up" : "nav-link"
+            }
+            onClick={() => handleNavStateChange("friends")}
+          >
+            Friends
+          </h3>
+          <h3
+            className={
+              navState === "calendar" ? "nav-link nav-link-grow-up" : "nav-link"
+            }
+            onClick={() => handleNavStateChange("calendar")}
+          >
+            Calendar
+          </h3>
+        </div>
+        <div className="sidebar-layout sidebar-right-hidden">
+          <div className="text-center">
+            <div className="flex justify-center items-center cursor-pointer">
+              <FontAwesomeIcon icon={faArrowRightFromBracket} />
+              <div className="ml-2">Logout</div>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <nav className="h-full nav-shadow nav-mobile">
+        <FontAwesomeIcon className="ml-6" size="2x" icon={faBars} />
         <Image
           src="/giftqueueLogo.png"
           width={"78"}
           height={"56"}
           alt={"giftqueue logo"}
         />
-      </div>
-      <div className="sidebar-layout sidebar-left-hidden"></div>
-      <div className="flex-1 flex justify-around handle-responsive-margin">
-        <h3
-          className={
-            navState === "giftqueue" ? "nav-link nav-link-grow-up" : "nav-link"
-          }
-          onClick={() => handleNavStateChange("giftqueue")}
-        >
-          Giftqueue
-        </h3>
-        <h3
-          className={
-            navState === "day" ? "nav-link nav-link-grow-up" : "nav-link"
-          }
-          onClick={() => handleNavStateChange("day")}
-        >
-          Celebrations
-        </h3>
-        <h3
-          className={
-            navState === "friends" ? "nav-link nav-link-grow-up" : "nav-link"
-          }
-          onClick={() => handleNavStateChange("friends")}
-        >
-          Friends
-        </h3>
-        <h3
-          className={
-            navState === "calendar" ? "nav-link nav-link-grow-up" : "nav-link"
-          }
-          onClick={() => handleNavStateChange("calendar")}
-        >
-          Calendar
-        </h3>
-      </div>
-      <div className="sidebar-layout sidebar-right-hidden">
-        <div className="text-center">
-          <div className="flex justify-center items-center cursor-pointer">
-            <FontAwesomeIcon icon={faArrowRightFromBracket} />
-            <div className="ml-2">Logout</div>
-          </div>
-        </div>
-      </div>
-    </nav>
+        <FontAwesomeIcon size="2x" className="gqp mr-6" icon={faBell} />
+      </nav>
+    </>
   );
 };
 
