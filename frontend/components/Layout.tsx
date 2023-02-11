862ccd27f9dfb50d61e728473c889d13cf40db76
Zander Ambrose <alexander.d.ambrose@gmail.com>
2023-02-11 13:42:11 -0500

    Get sticky nav in with icons.

--- frontend/components/Layout.tsx
+++ frontend/components/Layout.tsx
@@ -3,6 +3,14 @@ import ActivityFeedSidebar from "./ActivityFeedSidebar";
 import Maincontent from "./Maincontent";
 import Navigation from "./Navigation";
 import ProfileSidebar from "./ProfileSidebar";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faArrowRightFromBracket,
+  faCalendar,
+  faUserGroup,
+  faChampagneGlasses,
+  faGift,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Layout = () => {
   return (
@@ -20,6 +28,12 @@ const Layout = () => {
         <div className="sidebar-layout overflow-y-scroll sidebar-right-hidden">
           <ActivityFeedSidebar />
         </div>
+      </div>
+      <div className="bottom-mobile-nav">
+        <FontAwesomeIcon className="ml-10" size="2x" icon={faGift} />
+        <FontAwesomeIcon size="2x" icon={faChampagneGlasses} />
+        <FontAwesomeIcon size="2x" icon={faUserGroup} />
+        <FontAwesomeIcon className="mr-10" size="2x" icon={faCalendar} />
       </div>
     </>
   );
