25048fdd53a0f2358d7455b95161fc82ae1be88c
Zander Ambrose <alexander.d.ambrose@gmail.com>
2023-02-19 15:28:19 -0500

    Add min height to maincontetn container.

--- frontend/components/Maincontent.tsx
+++ frontend/components/Maincontent.tsx
@@ -17,7 +17,7 @@ const Maincontent = () => {
   const router = useRouter();
   return (
     <>
-      <main className="main-content-bg">
+      <main className="main-content-min main-content-bg">
         <div className="main-content-inner-container">
           {router.pathname == "/" ? (
             <>
