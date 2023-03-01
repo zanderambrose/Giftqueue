6431b1ef4c67135e329b5c823348f38536f811b9
Zander Ambrose <alexander.d.ambrose@gmail.com>
2023-03-01 12:06:55 -0500

    Error handle the contacts endpoint better.

--- frontend/pages/api/v1/contacts.ts
+++ frontend/pages/api/v1/contacts.ts
@@ -6,21 +6,25 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const token = await getToken({ req });
-  if (token) {
-    // Signed in
-    const response = await axios.get(
-      `https://people.googleapis.com/v1/people/me/connections?personFields=names,emailAddresses`,
-      //   `https://people.googleapis.com/v1/people/me?personFields=names,emailAddresses`,
-      {
-        headers: {
-          Authorization: `Bearer ${token.google_auth}`,
-          "Content-Type": "application/json",
-        },
-      }
-    );
-    res.status(200).json(response.data);
-  } else {
-    res.status(401);
+  try {
+    const token = await getToken({ req });
+    if (token) {
+      // Signed in
+      const response = await axios.get(
+        `https://people.googleapis.com/v1/people/me/connections?personFields=names,emailAddresses,phoneNumbers,clientData`,
+        //   `https://people.googleapis.com/v1/people/me?personFields=names,emailAddresses`,
+        {
+          headers: {
+            Authorization: `Bearer ${token.google_auth}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      res.status(200).json(response.data);
+    } else {
+      res.status(401).end();
+    }
+  } catch (error) {
+    res.status(401).end();
   }
 }
