2c30298f5bd7f6024507a0a3aebc23d83f1332cc
Zander Ambrose <alexander.d.ambrose@gmail.com>
2023-03-11 12:35:36 -0500

    Responsive style celebration and giftqueue modal delete items.

--- frontend/components/modals/ModalDeleteCelebrationItem.tsx
+++ frontend/components/modals/ModalDeleteCelebrationItem.tsx
@@ -35,7 +35,7 @@ const ModalDeleteGiftqueueItem = () => {
       {deleteItemModalShow.isOpen ? (
         <>
           <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-            <div className="relative w-5/12 my-6 mx-auto max-w-3xl">
+            <div className="relative w-12/12 md:w-10/12 lg:w-8/12 xl:w-6/12 my-6 mx-auto max-w-3xl">
               {/*content*/}
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                 {/*header*/}
