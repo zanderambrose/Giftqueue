13ffdd90476e5c48a622209d1cf9c5759038992f
Zander Ambrose <alexander.d.ambrose@gmail.com>
2023-03-11 12:39:56 -0500

    Fix width of giftqueue item modal.

--- frontend/components/modals/ModalGiftqueueItem.tsx
+++ frontend/components/modals/ModalGiftqueueItem.tsx
@@ -129,7 +129,7 @@ const ModalGiftqueueItem = () => {
       {giftqueueItemModalShow.isOpen ? (
         <>
           <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-            <div className="relative w-12/12 md:w-10/12 lg:w-8/12 xl:w-6/12 my-6 mx-auto max-w-3xl giftQueueModal">
+            <div className="relative w-11/12 md:w-10/12 lg:w-8/12 xl:w-6/12 my-6 mx-auto max-w-3xl giftQueueModal">
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                 {!isRelatedToViewOpen ? (
                   <>
