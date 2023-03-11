f28abf7fc53003c2d07fa603762daab403583ae1
Zander Ambrose <alexander.d.ambrose@gmail.com>
2023-03-11 14:27:46 -0500

    Extract activity feed data into its own component and pass into sidebar and sidebar mobile.

--- frontend/components/ActivityFeedSidebar.tsx
+++ frontend/components/ActivityFeedSidebar.tsx
@@ -1,52 +1,11 @@
-import React, { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faUserGroup,
-  faAngleDown,
-  faEarthAmericas,
-} from "@fortawesome/free-solid-svg-icons";
-import ActivitySidebarEventCard from "./ActivitySidebarEventCard";
-import ActivitySidebarFriendRequestCard from "./ActivitySidebarFriendRequestCard";
+import React from "react";
+import ActivityFeedData from "./activityFeed/ActivityFeedData";
 
 const ActivityFeedSidebar = () => {
-  const [activityData, setActivityData] = useState(false);
   return (
-    <div>
-      <div className="relative top-10 pb-8 border-b-2">
-        <div className="flex mx-auto items-center justify-center w-full">
-          <FontAwesomeIcon icon={faUserGroup} />
-          <p className="ml-2 text-base text-slate-500">Latest Updates</p>
-        </div>
-        <div className="flex justify-center items-center mt-4">
-          <div className="text-black text-sm text-center mr-2">Today</div>
-          <FontAwesomeIcon icon={faAngleDown} />
-        </div>
-      </div>
-      <div className="relative top-10 mt-8">
-        {activityData ? (
-          <>
-            <div className="flex justify-center items-center">
-              <FontAwesomeIcon
-                className="gqp"
-                size="4x"
-                icon={faEarthAmericas}
-              />
-            </div>
-            <p className="text-center mt-8 italic gqp">Nothing new yet!</p>
-          </>
-        ) : (
-          <div>
-            <ActivitySidebarFriendRequestCard />
-            <ActivitySidebarFriendRequestCard />
-            <ActivitySidebarEventCard />
-            <ActivitySidebarEventCard />
-            <ActivitySidebarEventCard />
-            <ActivitySidebarEventCard />
-            <ActivitySidebarEventCard />
-          </div>
-        )}
-      </div>
-    </div>
+    <>
+      <ActivityFeedData />
+    </>
   );
 };
 
