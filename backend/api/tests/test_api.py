583d6192f28e9f4b41e2b985f3621dd2d4d1fd25
Zander Ambrose <alexander.d.ambrose@gmail.com>
2023-06-18 09:51:08 -0400

    Add logging.

--- backend/api/tests/test_api.py
+++ backend/api/tests/test_api.py
@@ -1,11 +1,14 @@
 import pytest
+import logging
 from api.tests.fixtures.api import api_client
 
 from django.urls import reverse
 
+LOGGER = logging.getLogger(__name__)
 
 @pytest.mark.django_db
 def test_unauthorized_request(api_client):
    url = reverse('api:celebration-list')
    response = api_client.get(url)
+   LOGGER.info(f'response: {response}')
    assert response.status_code == 401
