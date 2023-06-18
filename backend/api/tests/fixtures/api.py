c7a8f9205dfae266bea3752aca7aabc1d38bf815
Zander Ambrose <alexander.d.ambrose@gmail.com>
2023-06-18 12:44:19 -0400

    Mock test user for api testing.

--- backend/api/tests/fixtures/api.py
+++ backend/api/tests/fixtures/api.py
@@ -5,3 +5,20 @@ import pytest
 def api_client():
    from rest_framework.test import APIClient
    return APIClient()
+
+@pytest.fixture
+def user_create(django_user_model):
+   def make_user(**kwargs):
+       return django_user_model.objects.create(
+       sub = 123456789,
+       email = 'testuser@testuser.com',
+       first_name = 'test',
+       last_name = 'user') 
+   return make_user
+
+@pytest.fixture
+def api_auth_client(user_create, api_client):
+    user = user_create()
+    api_client.force_authenticate(user=user)
+    yield api_client
+    api_client.force_authenticate(user=None)
--- backend/api/tests/test_api.py
+++ backend/api/tests/test_api.py
@@ -1,6 +1,6 @@
 import pytest
 import logging
-from api.tests.fixtures.api import api_client
+from api.tests.fixtures.api import api_client, api_auth_client, user_create
 
 from django.urls import reverse
 
@@ -10,5 +10,12 @@ LOGGER = logging.getLogger(__name__)
 def test_unauthorized_request(api_client):
    url = reverse('api:celebration-list')
    response = api_client.get(url)
-   LOGGER.info(f'response: {response}')
    assert response.status_code == 401
+
+@pytest.mark.django_db
+def test_authorized_request(api_auth_client):
+   url = reverse('api:celebration-list')
+   response = api_auth_client.get(url)
+   LOGGER.info(f'response: {response}')
+   LOGGER.info(f'response type: {type(response)}')
+   assert response.status_code == 200 
