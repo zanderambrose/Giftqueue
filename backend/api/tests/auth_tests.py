01bc1fb663922dceda948cb64685bdb9309189b6
Zander Ambrose <alexander.d.ambrose@gmail.com>
2023-06-21 08:45:13 -0400

    Doc string for auth_tests.

--- backend/api/tests/auth_tests.py
+++ backend/api/tests/auth_tests.py
@@ -10,18 +10,28 @@ LOGGER = logging.getLogger(__name__)
 
 @pytest.mark.django_db
 def test_unauthorized_request(api_client):
+   '''
+   Unathorized request should return 401 status code 
+   '''
    url = reverse('api:celebration-list')
    response = api_client.get(url)
    assert response.status_code == 401
 
 @pytest.mark.django_db
 def test_authorized_request(api_auth_client):
+   '''
+   Auth client request should return 200
+   Auth is being forced in api_auth_client fixture
+   '''
    url = reverse('api:celebration-list')
    response = api_auth_client.get(url)
    assert response.status_code == 200 
 
 @pytest.mark.django_db
 def test_user_name(api_auth_client):
+   '''
+   Auth client should create test user in database
+   '''
    url = reverse('api:celebration-list')
    response = api_auth_client.get(url)
    users = RegistryUser.objects.all()
