14cf20d065bfd8ba1e91c98cd9ef084c70ec07d0
Zander Ambrose <alexander.d.ambrose@gmail.com>
2023-06-20 20:39:27 -0400

    Celebration day payload shape test.

--- backend/api/tests/celebration_day_tests.py
+++ backend/api/tests/celebration_day_tests.py
@@ -1,9 +1,12 @@
 import pytest
+import logging
 
 from django.urls import reverse
 from rest_framework import status
 from api.models import CelebrationDay
 from faker import Faker
+
+LOGGER = logging.getLogger(__name__)
 
 fake = Faker()
 
@@ -17,6 +20,20 @@ def test_get_celebration_item(api_auth_client, create_celebration_day_payload):
    assert celebration_day['name'] == create_celebration_day_payload.name
    assert celebration_day['owner'] == create_celebration_day_payload.owner.id
    assert celebration_day['date'] == create_celebration_day_payload.date
+
+@pytest.mark.django_db
+def test_get_celebration_payload(api_auth_client, create_celebration_day_payload):
+   url = reverse('api:celebration-list')
+   response = api_auth_client.get(url)
+   celebration_day = list(response.data)
+   for day in celebration_day:
+       day_dict = dict(day)
+       assert 'id' in day_dict
+       assert 'created_at' in day_dict
+       assert 'updated_at' in day_dict
+       assert 'name' in day_dict
+       assert 'date' in day_dict
+       assert 'owner' in day_dict
 
 @pytest.mark.django_db
 def test_create_celebration_item(api_auth_client):
