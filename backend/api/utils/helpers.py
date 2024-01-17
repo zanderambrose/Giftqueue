bccee187ea56f4d016b021991fd3d7e2816978d6
Zander Ambrose <alexander.d.ambrose@gmail.com>
2024-01-16 20:15:33 -0500

    Fix broken test.

--- backend/api/utils/helpers.py
+++ backend/api/utils/helpers.py
@@ -1,9 +1,9 @@
 from django.http.request import QueryDict
 
 def append_owner_to_request_data(request):
-    if isinstance(request['data'], QueryDict):
-        request['data']['_mutable'] = True
-    request['data']['owner'] = request['user'].get('id')
+    if isinstance(request.data, QueryDict):
+        request.data._mutable = True
+    request.data['owner'] = request.user.id
     return request
 
 def gift_item_create_mapping(request):
