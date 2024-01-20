2a47f05f303dac83abfc7bd6def7d0f1d9fd4e2c
Zander Ambrose <alexander.d.ambrose@gmail.com>
2024-01-20 09:24:56 -0500

    Programatically replace year.

--- frontend/components/DateCard.tsx
+++ frontend/components/DateCard.tsx
@@ -1,32 +1,32 @@
 import React, { useState, useEffect } from "react";
 import date from "date-and-time";
 
-const DateCard = ({ date: celebrationDate }: { date: string }) => {
-  const [weekday, setWeekday] = useState("Mon");
-  const [month, setMonth] = useState("Jan");
-  const [dayNumber, setDayNumber] = useState("1");
-  useEffect(() => {
-    // Replace the years input with current year
-    // TODO - programatically add current year instead of hardcode 2023
-    const currentYearDate = celebrationDate?.replace(/^.{4}/g, "2023");
-    const formattedDate = date.format(
-      new Date(currentYearDate),
-      "ddd, MMM DD YYYY",
-      true
-    );
-    const dateValuesArr = formattedDate.split(" ");
-    setMonth(dateValuesArr[1]);
-    setDayNumber(dateValuesArr[2]);
-    setWeekday(formattedDate.substring(0, 3));
-  }, [celebrationDate]);
+const currentYear = new Date().getFullYear();
 
-  return (
-    <div className="dateCard">
-      <p>{weekday}</p>
-      <p className="text-lg font-black">{dayNumber}</p>
-      <p>{month}</p>
-    </div>
-  );
+const DateCard = ({ date: celebrationDate }: { date: string }) => {
+    const [weekday, setWeekday] = useState("Mon");
+    const [month, setMonth] = useState("Jan");
+    const [dayNumber, setDayNumber] = useState("1");
+    useEffect(() => {
+        const currentYearDate = celebrationDate?.replace(/^.{4}/g, String(currentYear));
+        const formattedDate = date.format(
+            new Date(currentYearDate),
+            "ddd, MMM DD YYYY",
+            true
+        );
+        const dateValuesArr = formattedDate.split(" ");
+        setMonth(dateValuesArr[1]);
+        setDayNumber(dateValuesArr[2]);
+        setWeekday(formattedDate.substring(0, 3));
+    }, [celebrationDate]);
+
+    return (
+        <div className="dateCard">
+            <p>{weekday}</p>
+            <p className="text-lg font-black">{dayNumber}</p>
+            <p>{month}</p>
+        </div>
+    );
 };
 
 export default DateCard;
