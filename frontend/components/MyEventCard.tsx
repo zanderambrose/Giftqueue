4ba2b2df74c8b80263c88efa7381b8331e727f13
Zander Ambrose <alexander.d.ambrose@gmail.com>
2024-01-20 09:32:05 -0500

    Adjust color for event card related to.

--- frontend/components/MyEventCard.tsx
+++ frontend/components/MyEventCard.tsx
@@ -3,8 +3,8 @@ import { faPen, faTrashCan } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useSetRecoilState } from "recoil";
 import {
-  defaultCelebrationDayModalState,
-  celebrationDayModal,
+    defaultCelebrationDayModalState,
+    celebrationDayModal,
 } from "../recoil/modal/celebrationDay";
 import { deleteCelebrationItemModal } from "../recoil/modal/deleteCelebrationItem";
 import { useQuery } from "@tanstack/react-query";
@@ -13,98 +13,98 @@ import { ModalRelatedGiftqueueItem } from "./modals/ModalRelatedGiftqueueItem";
 import { IGiftqueueSerializer } from "../util/typesClientApi";
 
 interface IEventCard {
-  name: string;
-  id: string;
-  canEdit?: boolean;
+    name: string;
+    id: string;
+    canEdit?: boolean;
 }
 
 const FriendEventCard = ({ name, id, canEdit = true }: IEventCard) => {
-  const setCelebrationDayModalShow = useSetRecoilState(celebrationDayModal);
-  const setDeleteItemShow = useSetRecoilState(deleteCelebrationItemModal);
+    const setCelebrationDayModalShow = useSetRecoilState(celebrationDayModal);
+    const setDeleteItemShow = useSetRecoilState(deleteCelebrationItemModal);
 
-  const handleEditItemClick = () => {
-    setCelebrationDayModalShow((currVal) => {
-      return {
-        ...currVal,
-        isOpen: true,
-        uuid: id,
-      };
+    const handleEditItemClick = () => {
+        setCelebrationDayModalShow((currVal) => {
+            return {
+                ...currVal,
+                isOpen: true,
+                uuid: id,
+            };
+        });
+    };
+    const handleDeleteItemClick = () => {
+        setDeleteItemShow((currVal) => {
+            return {
+                ...currVal,
+                isOpen: true,
+                uuid: id,
+            };
+        });
+    };
+
+    const [relatedItems, setRelatedItems] = useState(0);
+    const [relatedItemsData, setRelatedItemsData] =
+        useState<IGiftqueueSerializer[]>();
+    const { getGiftqueueItems } = useGiftqueueApi();
+    useQuery({
+        queryKey: ["myGiftqueueItems"],
+        queryFn: getGiftqueueItems,
+        onSuccess: (data) => {
+            const totalNumber = data.filter((item) => {
+                return item.related_to?.id === id;
+            });
+            setRelatedItems(totalNumber.length);
+            setRelatedItemsData(totalNumber);
+        },
     });
-  };
-  const handleDeleteItemClick = () => {
-    setDeleteItemShow((currVal) => {
-      return {
-        ...currVal,
-        isOpen: true,
-        uuid: id,
-      };
-    });
-  };
 
-  const [relatedItems, setRelatedItems] = useState(0);
-  const [relatedItemsData, setRelatedItemsData] =
-    useState<IGiftqueueSerializer[]>();
-  const { getGiftqueueItems } = useGiftqueueApi();
-  useQuery({
-    queryKey: ["myGiftqueueItems"],
-    queryFn: getGiftqueueItems,
-    onSuccess: (data) => {
-      const totalNumber = data.filter((item) => {
-        return item.related_to?.id === id;
-      });
-      setRelatedItems(totalNumber.length);
-      setRelatedItemsData(totalNumber);
-    },
-  });
+    const [relatedGiftqueueModal, setRelatedGiftqueueModal] = useState(false);
 
-  const [relatedGiftqueueModal, setRelatedGiftqueueModal] = useState(false);
-
-  return (
-    <>
-      <div className="friendEventCard">
-        <div className="w-full mx-6 flex flex-row justify-between items-center">
-          <div>
-            <h2 className="font-black text-lg">{name}</h2>
-            {relatedItems > 0 && (
-              <p className="text-sm">
-                <span
-                  onClick={() => setRelatedGiftqueueModal(true)}
-                  className="text-blue-600 cursor-pointer hover:opacity-80"
-                >
-                  {relatedItems}{" "}
-                </span>
-                items added to my wishlist
-              </p>
-            )}
-          </div>
-          {canEdit && (
-            <div>
-              <FontAwesomeIcon
-                onClick={handleDeleteItemClick}
-                size="lg"
-                className="muted mr-6 hover:opacity-80"
-                icon={faTrashCan}
-              />
-              <FontAwesomeIcon
-                onClick={handleEditItemClick}
-                size="lg"
-                className="gqp hover:opacity-80"
-                icon={faPen}
-              />
+    return (
+        <>
+            <div className="friendEventCard">
+                <div className="w-full mx-6 flex flex-row justify-between items-center">
+                    <div>
+                        <h2 className="font-black text-lg">{name}</h2>
+                        {relatedItems > 0 && (
+                            <p className="text-sm">
+                                <span
+                                    onClick={() => setRelatedGiftqueueModal(true)}
+                                    className="text-blue-400 hover:text-blue-700 cursor-pointer hover:opacity-80"
+                                >
+                                    {relatedItems}{" "}
+                                </span>
+                                items added to my wishlist
+                            </p>
+                        )}
+                    </div>
+                    {canEdit && (
+                        <div>
+                            <FontAwesomeIcon
+                                onClick={handleDeleteItemClick}
+                                size="lg"
+                                className="muted mr-6 hover:opacity-80"
+                                icon={faTrashCan}
+                            />
+                            <FontAwesomeIcon
+                                onClick={handleEditItemClick}
+                                size="lg"
+                                className="gqp hover:opacity-80"
+                                icon={faPen}
+                            />
+                        </div>
+                    )}
+                </div>
             </div>
-          )}
-        </div>
-      </div>
-      {relatedItemsData && (
-        <ModalRelatedGiftqueueItem
-          isOpen={relatedGiftqueueModal}
-          setIsOpen={setRelatedGiftqueueModal}
-          giftqueueItemData={relatedItemsData}
-          name={name}
-        />
-      )}
-    </>
-  );
+            {relatedItemsData && (
+                <ModalRelatedGiftqueueItem
+                    isOpen={relatedGiftqueueModal}
+                    setIsOpen={setRelatedGiftqueueModal}
+                    giftqueueItemData={relatedItemsData}
+                    name={name}
+                />
+            )}
+        </>
+    );
 };
 
 export default FriendEventCard;
