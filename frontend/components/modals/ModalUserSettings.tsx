a3d6d7754371d8df887da5e5449501066ec1bd6b
Zander Ambrose <alexander.d.ambrose@gmail.com>
2024-01-24 19:51:54 -0500

    Get profile image avatar working.

--- frontend/components/modals/ModalUserSettings.tsx
+++ frontend/components/modals/ModalUserSettings.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { useSession } from "next-auth/react";
 import { useRecoilState } from "recoil";
 import {
@@ -25,7 +25,8 @@ export const ModalUserSettings = () => {
         formState: { errors, isSubmitting },
     } = useForm<ModalUserSettingsInputs>();
 
-    const [profileImage, setProfileImage] = useState(false)
+    const [profileImage, setProfileImage] = useState<File | false>(false)
+    const fileInputRef = useRef<HTMLInputElement>(null);
     const { data: session } = useSession();
     const { getUserSettings, updateUserSettings } = useUserSettings()
     const queryClient = useQueryClient();
@@ -78,6 +79,24 @@ export const ModalUserSettings = () => {
         setProfileImage(file);
     };
 
+    const imgAvatar = () => {
+        if (profileImage) {
+            return URL.createObjectURL(profileImage)
+        }
+        if (userSettingsData && userSettingsData.profile_image) {
+            return `${process.env.NEXT_PUBLIC_REGISTRY_BASE_URL}${userSettingsData.profile_image}`
+        }
+        return session?.user?.image ?? ""
+
+    }
+
+    const clearFileInput = () => {
+        if (fileInputRef.current) {
+            fileInputRef.current.value = '';
+            setProfileImage(false)
+        }
+    };
+
     return (
         <>
             {userSettingsModalShow.isOpen ? (
@@ -90,7 +109,7 @@ export const ModalUserSettings = () => {
                             <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                                 {/*header*/}
                                 <div className="flex items-start justify-between p-5">
-                                    <h3 className="text-lg font-semibold">User Settings</h3>
+                                    <h3 className="text-lg font-semibold">Profile Settings</h3>
                                     <button
                                         className="p-1 ml-auto bg-transparent border-0 text-black float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
                                         onClick={handleModalReset}
@@ -111,28 +130,35 @@ export const ModalUserSettings = () => {
                                         <input
                                             {...register("display_name")}
                                             type="text"
+                                            ref={fileInputRef}
                                             placeholder={displayName()}
                                             className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-md shadow-sm placeholder-slate-400
       focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500"
                                         />
-                                        <label className="block mt-8">
-                                            <span className="block text-md font-medium">
-                                                Profile Image
-                                            </span>
-                                        </label>
-                                        <input
-                                            onChange={handleFileChange}
-                                            type="file"
-                                            name="profile_image"
-                                            id="profile_image"
-                                            className="text-sm text-stone-500
+                                        <div className="flex items-end mt-4">
+                                            <div>
+                                                <label className="block">
+                                                    <span className="block text-md font-medium">
+                                                        Profile Image
+                                                    </span>
+                                                </label>
+                                                <input
+                                                    onChange={handleFileChange}
+                                                    type="file"
+                                                    name="profile_image"
+                                                    id="profile_image"
+                                                    className="text-sm text-stone-500
                                                file:mr-5 file:py-1 file:px-3 file:border-[1px]
                                                file:text-xs file:font-medium
                                                file:bg-stone-50 file:text-stone-700
                                                hover:file:cursor-pointer hover:file:bg-blue-50
                                                hover:file:text-blue-700"
-                                        >
-                                        </input>
+                                                >
+                                                </input>
+                                            </div>
+                                            <img src={imgAvatar()} alt="" height="100" width="100" className="rounded border" />
+                                        </div>
+                                        <button className="p-2 mt-2  border" type="button" onClick={clearFileInput}>Remove image</button>
                                         <div className="text-center block p-6">
                                             <button
                                                 className="main-Btn hover:opacity-80"
